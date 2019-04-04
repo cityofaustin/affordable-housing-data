1455026a9cca69922bf977cd71a966b703963a16
Frederick Shotlow <frederick.shotlow@austintexas.gov>
2019-04-04 13:39:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -19,12 +19,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV == 'Production') {
-    console.log('production')
     // serve the react app file
-    //app.use(express.static(`${__dirname}/client/build`))
     app.use(express.static(path.join(__dirname, 'client/build')));
-    //app.use(express.static('client/build'))
-    //app.use(express.static('client/build'))
 }
 
 // app.use(cookieParser());
