955da392bf0f99c73e72df4ae6e51b081abae1c5
D Lu <danhao.lu@austintexas.gov>
2019-05-09 21:36:13 -0500

    update registration link

--- routes/index.js
+++ routes/index.js
@@ -208,13 +208,14 @@ router.post('/logout', async (req, res) => {
     }
 })
 
-
-router.get('/registration', async (req, res) => {
+router.get('/checkuser', async (req, res) => {
     try {
-        if (!await sessionHelper.isAuthorized(req.query.userEmail, req.sessionID)) {
+        if (await sessionHelper.isAuthorized(req.query.userEmail, req.sessionID)) {
+            return res.status(200).send({success: true, redirect: null})
+        } else {
             return res.status(200).send({success: false, redirect: '/'})
         }
-        return res.status(200).send({success: true});
+        //return res.status(200).send({success: true});
     } catch (e) {
         logger.log('error', e, {origin: 'server'});
         return res.status(500).send({success: false, error: e.stack.toString(), serverSideError: true});
